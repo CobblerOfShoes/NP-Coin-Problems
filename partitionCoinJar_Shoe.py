ba77e8fc9ba690141093608ffd4a8f5cc139311b
CobblerOfShoes <ajslbm804@gmail.com>
2024-10-18 11:50:10 -0400

    Initial completion of code and test cases

---
+++ partitionCoinJar_Shoe.py
@@ -0,0 +1,161 @@
+from math import inf
+import os;
+import argparse;
+import csv
+import time;
+import matplotlib.pyplot as plt;
+
+def is_file_valid(filename) -> bool:
+  if not os.path.exists(filename):
+    print(f"The file {filename} could not be found")
+    return False
+  return True
+
+# Try with itertools?
+def dumbSolver(jarOfCoins: list[int], index: int, leftPile: list[int], rightPile: list[int]) \
+-> list[list[int], list[int]]:
+  # Base Case: only one coin is left, and we no longer recur
+  if (len(jarOfCoins) == index + 1):
+    lastCoin = jarOfCoins[index]
+    # Compute the value of the left and right piles
+    leftSum = sum(leftPile)
+    rightSum = sum(rightPile)
+    # Try and balance both piles
+    if ((leftSum + lastCoin) == rightSum):
+      return [leftPile + [lastCoin], rightPile]
+    if ((rightSum + lastCoin) == leftSum):
+      return [leftPile, rightPile + [lastCoin]]
+    # If no pairing works, return empty for failure
+    return []
+  
+  # Otherwise, try putting the coin in the left and right pile
+  nextCoin = jarOfCoins[index]
+
+  tryLeftPile = dumbSolver(jarOfCoins, index+1, leftPile + [nextCoin], rightPile)
+  if (tryLeftPile): 
+    return tryLeftPile
+  
+  tryRightPile = dumbSolver(jarOfCoins, index+1, leftPile, rightPile + [nextCoin])
+  if (tryRightPile): 
+    return tryRightPile
+    
+  # If no combos worked, return empty for failure
+  return []
+
+'''
+Finds a combination of the given coin amounts that sum to the given total.
+Returns none if no combination is possible.
+Inputs: list[testcaseType, totalValue, coin1, coin2, coin3...]'''
+def find_coin_counts(testcaseNumber: int, jarOfCoins: list[int]) \
+-> dict[int: tuple[list[list[int], list[int]], float, int]]:
+  # Get the number of coins to classify the problem
+  numCoins = len(jarOfCoins)
+
+  # Start the clock to measure program length
+  startTime_ns = time.time_ns()
+
+  # Use a backtracking algorithm to find the amount of each coin to use
+  splitPiles = dumbSolver(jarOfCoins, 0, [], [])
+
+  # Stope the clock
+  endTime_ns = time.time_ns()
+  totalTime_ns = endTime_ns - startTime_ns
+  totalTimeMicroseconds = totalTime_ns
+
+  # leftPile = []
+  # rightPile = []
+  # # If the solver was successful, then retrieve both piles
+  # if (splitPiles):
+  #   leftPile = splitPiles[0]
+  #   rightPile = splitPiles[1]
+
+  # Generate testcase result
+  return {testcaseNumber: (splitPiles, totalTime_ns, numCoins)}
+
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument("-f", dest="filename", required=True,
+                      help="Input file with knapsack problem test cases." + "\n" +
+                           "Data should be in the format: TestcaseNo., Coin1,  Coin2, Coin3...",
+                      metavar="FILE")
+  parser.add_argument("-o", "--out-file", dest="outfile", required=True,
+                      help="File to dump results into", metavar="FILE")
+  args = parser.parse_args()
+
+  # Check if the file exists
+  if (not is_file_valid(args.filename)):
+    return
+  
+  # Create a dictionary to store results
+  allResults: dict[int: tuple[list[list[int], list[int]], float, int]] = {}
+  successfulResults: dict[int: tuple[list[list[int], list[int]], float, int]] = {}
+  failureResults: dict[int: tuple[list[list[int], list[int]], float, int]] = {}
+
+  # Create a min and max of problem size (will be useful later for results)
+  # minCoinAmount = inf
+  # maxCoinAmount = 0
+
+  # Parse the input file
+  with open(args.filename, newline='') as csvfile:
+    testcases = csv.reader(csvfile, delimiter=' ')
+    # Loop through the testcases and generate solutions
+    for test in testcases:
+      # Convert data to a list of ints and grab the test case number
+      testCaseInput = list(map(int, test))
+      testCaseNumber = testCaseInput.pop(0)
+      # Adujust min and max
+      # numCoins = len(testCaseInput)
+      # if numCoins < int(minCoinAmount):
+      #   minCoinAmount = numCoins
+      # elif numCoins > maxCoinAmount:
+      #   maxCoinAmount = numCoins
+      # Attempt to compute a solution
+      testCaseResults = find_coin_counts(testCaseNumber, testCaseInput)
+      allResults.update(testCaseResults)
+      # Classify as a success or failure
+      if (not testCaseResults[testCaseNumber][0] or testCaseResults[testCaseNumber][0] == {}):
+        failureResults.update(testCaseResults)
+        continue
+      successfulResults.update(testCaseResults)
+
+  with open(args.outfile, 'w') as outputFile:
+    # Print column labels
+    outputFile.write("Testcase_Number Left_Coin_Pile Right_Coin_Pile Computation_Time Number_of_Coins\n")
+    # Print out testcase data
+    for testcase in range(1, testCaseNumber + 1):
+      outputFile.write(f"{testcase}")
+      try:
+        leftPiles = ','.join(list(map(str,allResults[testcase][0][0])))
+        if (leftPiles == []):
+          outputFile.write(" 0")
+          continue
+        outputFile.write(f" {leftPiles}")
+      except IndexError:
+        outputFile.write(" 0")
+      try:
+        rightPiles = ','.join(list(map(str,allResults[testcase][0][1])))
+        if (rightPiles == []):
+          outputFile.write(" 0")
+          continue
+        outputFile.write(f" {rightPiles}")
+      except IndexError:
+        outputFile.write(" 0")
+      outputFile.write(f" {allResults[testcase][1]}")
+      outputFile.write(f" {allResults[testcase][2]}")
+      outputFile.write("\n")
+
+
+  # Get the x and y values for successful results (x = numCoins, y = time elapsed)
+  successXPoints = [value[2] for value in successfulResults.values()]
+  successYPoints = [value[1] for value in successfulResults.values()]
+
+  # Get x and y for failures
+  failureXPoints = [value[2] for value in failureResults.values()]
+  failureYPoints = [value[1] for value in failureResults.values()]
+
+  plt.plot(successXPoints, successYPoints, 'o')
+  plt.plot(failureXPoints, failureYPoints, 'x')
+  plt.show()
+
+if __name__ == '__main__':
+  main()
---
+++ partitionTestCaseGenerator_Shoe.py
@@ -0,0 +1,68 @@
+import argparse;
+import random;
+from datetime import datetime
+import textwrap;
+
+def main():
+  # Handle arguments
+  parser = argparse.ArgumentParser(
+    prog='knapsackTestCaseGen',
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+    description='Creates a given number each of small, medium, and large test cases for the knapsack problem.',
+    epilog=textwrap.dedent('''
+        The range for total value, number of coins, and coin size can be configured within the code.
+        The cases are in format "totalvalue coin1value coin1count coin2value coint2count...'''))
+  
+  parser.add_argument("--file", "-f", required=True, type=argparse.FileType('w'),
+                      help="File to print test cases to (Will overwrite data).")
+  parser.add_argument("--size", "-s", required=True, type=int,
+                      help="Number of test cases of to generate for each amount of coins.")
+  parser.add_argument("--number-coins", "-n", required=True, type=int, dest='maxNumCoins',
+                      help="Maximum number of coins (must be 3 or more).")
+  
+  args = parser.parse_args()
+
+  # If something went wrong and the file cannot be opened, exit with error
+  if (not args.file):
+    print("Error opening file.")
+    return
+  
+  # Otherwise, print test cases to file
+
+  # Set a total value range
+  maxCoinValue = 25
+
+  # Set the maximum number of coin values to generate from input
+  maxNumCoins = args.maxNumCoins
+
+  # Set a test case counter
+  testcaseNumber = 0
+
+  # Set randomization seed
+  random.seed(datetime.now().timestamp())
+  # Loop through the given range of coins
+  # We start with three coins because this problem is trivial with 2 coins
+  for numCoins in range(3, maxNumCoins + 1):
+    # Loop through the given number of test cases for each coin quantity
+    #maxCoinQuantity = 50 * numCoins
+    for test in range(args.size):
+      # Write the test case number
+      testcaseNumber += 1
+      args.file.write(f"{testcaseNumber}")
+      # For each coin, generate a random coin value and amount of that coin
+      for coin in range(numCoins):
+        # Write the coin value
+        # We do not include pennies as they somewhat trivialize this problem
+        args.file.write(f" {random.randint(2, maxCoinValue)}")
+        # Write the coin quantity
+        # args.file.write(f" {random.randint(1, maxCoinQuantity)}")
+        #args.file.write(f" {random.randint(10, maxCoinQuantity)}")
+        # args.file.write(f" {maxCoinQuantity}")
+      # Move to the next line
+      args.file.write("\n")
+
+  # Close the file once we are done
+  args.file.close()
+
+if __name__ == '__main__':
+  main()
